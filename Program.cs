f170f9e2c8fe4fb294e9c711d712454ff76a797b
chanan-duy <chanan.duy@proton.me>
2025-09-07 16:18:05 +0500

    Run Fibonacci 10 numbers

--- Program.cs
+++ Program.cs
@@ -1,9 +1,35 @@
 ﻿namespace MP_KT15_2025;
 
+public static class Logic
+{
+    private static void PrintNewLine()
+    {
+        Console.Write(Environment.NewLine);
+    }
+
+    private static void RunFibonacci()
+    {
+        var fib = new Fibonacci(10);
+        var counter = 1;
+
+        foreach (var num in fib)
+        {
+            Console.WriteLine($"{counter}. {num}");
+            counter += 1;
+        }
+    }
+
+    public static void Run()
+    {
+        RunFibonacci();
+        PrintNewLine();
+    }
+}
+
 public static class Program
 {
     public static void Main(string[] _)
     {
-        Console.WriteLine("Hello, World!");
+        Logic.Run();
     }
 }
