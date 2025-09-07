79f435c18983c75dc546c7c6ad3477a9da4f6aab
chanan-duy <chanan.duy@proton.me>
2025-09-07 16:17:51 +0500

    Init Fibonacci IEnumerable

---
+++ Fibonacci.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+namespace MP_KT15_2025;
+
+public class Fibonacci : IEnumerable<int>
+{
+    public int NumbersAmount { get; set; }
+
+    public Fibonacci(int numbersAmount)
+    {
+        NumbersAmount = numbersAmount;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        var index = 0;
+
+        if (index++ >= NumbersAmount)
+        {
+            yield break;
+        }
+
+        yield return 0;
+
+        if (index++ >= NumbersAmount)
+        {
+            yield break;
+        }
+
+        yield return 1;
+
+        var first = 0;
+        var second = 1;
+
+        while (index < NumbersAmount)
+        {
+            var current = first + second;
+            (first, second) = (second, current);
+            index += 1;
+            yield return current;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
